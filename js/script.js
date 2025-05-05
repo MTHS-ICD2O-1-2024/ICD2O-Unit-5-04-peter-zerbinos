3c4ca7f933756c1382b5c88e75e47729744cd86c
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-05 02:03:57 +0000

    added changes to index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use scrict"
+
+function checkForDiscount() {
+  const userAge = parseInt(document.getElementById('user-age').value)
+  const dayOfWeek = document.getElementById('day-of-week').value
+
+  // process
+  if ((dayOfWeek == 'TUE' || dayOfWeek == 'THU') || (userAge > 12 && userAge < 21)) {
+    document.getElementById('result').innerHTML =
+      '<p>You are eligible for student pricing!</p>'
+  }
+  else {
+    document.getElementById('result').innerHTML =
+      '<p>You must pay the regular price.</p>'
+  }
+}
